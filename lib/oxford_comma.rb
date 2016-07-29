569753440f57acaf462488def7c97a0974160f10
Wesley Friedman <friedman.wesley@gmail.com>
2016-07-29 22:11:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    array[array.length - 1] = "and " << array.last
+    return array.join(", ")
+  end
 end
